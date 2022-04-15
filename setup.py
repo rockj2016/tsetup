9d60014b154d6ed01442654efb128f04df72b9a7
jiangyuan <jiangyuan@quseit.com>
2022-04-15 15:35:17 +0800

    refactor

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='Tsetup',
     version='0.0.1',
-    packages=['requests'],
+    packages=['main'],
     long_description=open('README.md').read(),
 )
